bf58693bfc42c32ee0ec459b5e06582c6bdadbdc
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2026-03-06 19:42:46 -0500

    se elimina componente arrow

--- src/app/components/icons/arrow/arrow.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-arrow',
-  imports: [],
-  templateUrl: './arrow.html',
-  styleUrl: './arrow.css',
-})
-export class Arrow {}
