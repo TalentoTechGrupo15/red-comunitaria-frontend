c6b123f772a6474f3e7ccd09b6b0e11f36512dd0
= <riveromauro@hotmail.com>
2026-03-09 22:20:47 -0500

    add login component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { Routes } from '@angular/router';
 import { EquipoComponent } from './components/equipo.component/equipo.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
     {
-        path:"equipo", component: EquipoComponent
+        path: '', 
+        redirectTo: '/login', 
+        pathMatch: 'full' 
+    },
+    {
+        path: 'login', 
+        component: LoginComponent
+    },
+    {
+        path: 'equipo', 
+        component: EquipoComponent
+    },
+    {
+        path: '**', 
+        redirectTo: '/login'  
     }
-
 ];
